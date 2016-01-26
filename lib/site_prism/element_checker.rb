aec1f5616c768f2c4bf11a01e2cef63a7fe1a4fb
Betsy Haibel <betsy@actbluetech.com>
2016-01-26 17:20:54 -0500

    add implicit waits to the #all_there? check if they're enabled

--- lib/site_prism/element_checker.rb
+++ lib/site_prism/element_checker.rb
@@ -1,7 +1,8 @@
 module SitePrism
   module ElementChecker
     def all_there?
-      Capybara.using_wait_time(0) do
+      wait_time = SitePrism.use_implicit_waits ? Waiter.default_wait_time : 0
+      Capybara.using_wait_time(wait_time) do
         self.class.mapped_items.all? { |element| send "has_#{element}?" }
       end
     end
